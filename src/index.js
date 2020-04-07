b71efd268d136ce662b44ea9636be50bf8cac1ca
Inhoi <mkmdivy@gmail.com>
2020-04-08 00:28:32 +0200

    'iamgod'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
 import papa from 'papaparse';
-
+import csv2geojson from 'csv2geojson'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWttZCIsImEiOiJjajBqYjJpY2owMDE0Mndsbml0d2V1ZXczIn0.el8wQmA-TSJp2ggX8fJ1rA';
 class Application extends React.Component {
@@ -73,16 +73,56 @@ class Application extends React.Component {
     });
     });
 
-    //axios.get('chad.json').then(response=>{console.log(response.data.Agglomerations[0].Longlat)})
+    map.on('load', function() {
+      // Add a circle layer with a vector source.
+      map.addLayer({
+        id: 'points-of-interest',
+        source: {
+          type: 'vector',
+          url: 'mapbox://mkmd.ck5qy6ole1xja2npflwvfk78l-2j015'
+        },
+        'source-layer': 'metropole',
+        type: 'circle',
+        paint: {
+          // Mapbox Style Specification paint properties
+        },
+        layout: {
+          // Mapbox Style Specification layout properties
+        }
+      });
 
+    });
+    //axios.get('chad.json').then(response=>{console.log(response.data.Agglomerations[0].Longlat)})
+    //mkmd.ck5qy6ole1xja2npflwvfk78l-2j015
     async function makeGetRequest(country,select) {
       let covid = await axios.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
       //console.log(covid.data)
-      let covid_array= papa.parse(covid.data,{header:true,skipEmptyLines:true}).data;
-      var x;
-      for (x in covid_array){new mapboxgl.Marker().setLngLat([covid_array[x].Long,covid_array[x].Lat]).addTo(map);}
-
+      csv2geojson.csv2geojson(covid.data,function(err,data){let covid_array=data
       console.log(covid_array)
+      map.addSource('covid', {
+        type: 'geojson',
+        data: covid_array
+      });
+      map.addLayer({
+      'id': 'covid',
+      'type': 'circle',
+      'source': 'covid',
+      'paint': {
+      //'fill-color': 'rgba(200, 100, 240, 0.4)',
+      //'fill-outline-color': 'rgba(200, 100, 240, 1)'
+      }
+      });
+        //new mapboxgl.Marker().setLngLat([covid_array[x].Long,covid_array[x].Lat]).addTo(map);
+      });
+
+
+      //let covid_array= papa.parse(covid.data,{header:true,skipEmptyLines:true}).data;
+      //var x;
+      //for (x in [3,5,25,32,33,35,47,48,84,85,87,96,99,101,104,105,118,119,122,125,143,149,153,157,164,165,173,174,189,194,200,201,204,209,211,213,215,230,231,235,241,243,244,254,255,256,258,261,262,263]){new mapboxgl.Marker().setLngLat([covid_array[x].Long,covid_array[x].Lat]).addTo(map)
+        //new mapboxgl.Marker().setLngLat([covid_array[x].Long,covid_array[x].Lat]).addTo(map);
+      //}
+
+
       let res = await axios.get(country+'.json');
       //console.log(res.data.Agglomerations);
       let data = res.data.Agglomerations[select].Longlat;
